e1141711873adcfe092e8532f50a0d0a928be1c9
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2020-10-09 22:01:46 -0300

    started the componentization of the page's header, menu, content and footer.

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <span>Copyrght Capgemini © 2020</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name:'Footer'
+}
+</script>
+
+<style >
+  .footer {
+    grid-area: footer;
+    background-color: #DDD;
+    color:#333;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>

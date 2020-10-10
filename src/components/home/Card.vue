d7e0c3d67def1bbff11e30c2f362ec520ca49862
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2020-10-10 14:24:27 -0300

    basic home structure completed

---
+++ src/components/home/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="cards">
+    <div class="cards-icon">
+      <i :class="icon" :style="style"></i>
+    </div>
+    <div class="cards-info">
+      <span class="cards-title">{{title}}</span>
+      <span class="cards-value">{{value}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['title', 'value', 'icon', 'color'],
+  computed: {
+    style() {
+      return "color: " + (this.color || '#000')
+    }
+  }
+}
+</script>
+
+ <style>
+    .cards {
+        flex: 1;
+        display: flex;
+        border-radius: 8px;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        padding: 20px;
+
+    }
+    .cards-icon {
+        display: flex;
+        align-items: center;
+    }
+    .cards-icon i {
+        font-size: 5rem;
+    }
+    .cards-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+    .cards-title {
+        font-size: 1.2rem;
+    }
+    .cards-value {
+        font-size: 2rem;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] =
+ 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL0dpdEh1YlwvY2FwZ2VtaW5pLWFwaVwvcHVibGljXC9hcGlcL2FjY291bnRcL2xvZ2luIiwiaWF0IjoxNjAyMzQ5NTE3LCJleHAiOjE2MDIzNTMxMTcsIm5iZiI6MTYwMjM0OTUxNywianRpIjoiUWtYd2NXU2JhNUtxa2h3OCIsInN1YiI6MSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9._R7zgqIvFlZU2znR_ovfx-feP5yUpQxv5yAlMyTaza4'
+
 new Vue({
   store,
   router,

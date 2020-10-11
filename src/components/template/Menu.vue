4d6b04a735a27cb74bc46e21f5bd9960f1194863
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2020-10-11 14:53:29 -0300

    inserted button link menu

--- src/components/template/Menu.vue
+++ src/components/template/Menu.vue
@@ -1,5 +1,10 @@
 <template>
-  <aside class="menu" v-show="isMenuVisible"></aside>
+  <aside class="menu" v-show="isMenuVisible">
+    <div class="submenu">
+      <router-link to="/admin/deposits" class="mb-2 mt-2">Saque</router-link>
+      <router-link to="/admin/withdraws" class="mb-2">Depósito</router-link>
+    </div>
+  </aside>
 </template>
 
 <script>
@@ -21,4 +26,27 @@ export default {
     flex-direction: column;
     flex-wrap: wrap;
   }
+
+  .submenu {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding:10px;
+    text-decoration: none;
+  }
+
+  .submenu a {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    color: #fff;
+  }
+
+  .submenu a:hover {
+    text-decoration: none;
+    color: #000;
+    background-color: #ccc;
+  }
+
+
 </style>
